584e996b036d53a5dd029045e8676bdbc1ce2270
Samadali123 <samadali0125@gmail.com>
2024-03-17 16:19:07 +0530

    add a comment feature in App

--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,34 @@ const userModel = require(`./users`);
 const postModel = require(`./post`);
 const commentModel = require(`./comments`)
 const utils = require(`../utils/utils`);
-const { populate } = require('dotenv');
+
 
 
 passport.use(new localStrategy(userModel.authenticate()));
 
 router.post(`/register`, function(req, res, next) {
-    var newUser = new userModel({
-        username: req.body.username,
-        fullname: req.body.fullname,
-        email: req.body.email,
-    })
 
-    userModel.register(newUser, req.body.password)
-        .then(function(e) {
-            passport.authenticate(`local`)(req, res, function() {
-                res.redirect(`/profile`);
-            })
+    try {
+        var newUser = new userModel({
+            username: req.body.username,
+            fullname: req.body.fullname,
+            email: req.body.email,
         })
 
-    .catch(function(err) {
-        res.send(err);
-    })
+        userModel.register(newUser, req.body.password)
+            .then(function(e) {
+                passport.authenticate(`local`)(req, res, function() {
+                    res.redirect(`/profile`);
+                })
+            })
+
+        .catch(function(err) {
+            res.status(403).json({ message: "Details is necessary for creating an account" })
+        })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+
 
 })
 
@@ -41,18 +47,30 @@ router.post(`/login`, passport.authenticate(`local`, {
 
 
 router.get('/logout', IsLoggedIn, function(req, res, next) {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
+    try {
+        req.logout(function(err) {
+            if (err) { return next(err); }
+            res.redirect('/');
+        });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message })
+
+    }
+
 });
 
 
 function IsLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
+    try {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect(`/login`);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message })
     }
-    res.redirect(`/login`);
+
+
 }
 
 
@@ -66,134 +84,201 @@ router.get('/login', function(req, res) {
 });
 
 router.get('/feed', IsLoggedIn, async function(req, res) {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user });
-    const allposts = await postModel.find().populate(`user`).populate(`comments`);
-    res.render('feed', { footer: true, loginuser, allposts, dater: utils.formatRelativeTime, });
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user });
+        const allposts = await postModel.find().populate(`user`).populate(`comments`);
+        res.render('feed', { footer: true, loginuser, allposts, dater: utils.formatRelativeTime, });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 });
 
 router.get('/profile', IsLoggedIn, async function(req, res) {
+    try {
 
-    const loginuser = await userModel.findOne({ username: req.session.passport.user }).populate(`posts`);
-    res.render('profile', { footer: true, loginuser });
+        const loginuser = await userModel.findOne({ username: req.session.passport.user }).populate(`posts`);
+        res.render('profile', { footer: true, loginuser });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 });
 
 
 router.post(`/uploadprofile`, IsLoggedIn, upload.single(`profile`), async(req, res, next) => {
 
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
-    loginuser.profile = req.file.filename;
-    await loginuser.save();
-    res.redirect(`/edit`);
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        loginuser.profile = req.file.filename;
+        await loginuser.save();
+        res.redirect(`/edit`);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 
 })
 
 router.post(`/update`, async(req, res) => {
-    const user = await userModel.findOneAndUpdate({ username: req.session.passport.user }, { username: req.body.username, fullname: req.body.name, bio: req.body.bio }, { new: true });
+    try {
+        const user = await userModel.findOneAndUpdate({ username: req.session.passport.user }, { username: req.body.username, fullname: req.body.name, bio: req.body.bio }, { new: true });
+        req.logIn(user, function(err) {
+            if (err) throw err;
+            res.redirect(`/profile`);
+        })
+    } catch (err) {
 
-    req.logIn(user, function(err) {
-        if (err) throw err;
-        res.redirect(`/profile`);
-    })
+        res.status(500).json({ success: false, message: err.message });
+    }
+
+
+
 })
 
 
 
 router.get('/search', IsLoggedIn, async function(req, res) {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
-    res.render('search', { footer: true, loginuser });
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        res.render('search', { footer: true, loginuser });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 });
 
 
 router.get(`/users/:input`, IsLoggedIn, async(req, res) => {
-    const input = req.params.input;
-    const regex = new RegExp(`^${input}`, 'i');
-    const users = await userModel.find({ username: regex });
-    res.json(users);
+    try {
+        const input = req.params.input;
+        const regex = new RegExp(`^${input}`, 'i');
+        const users = await userModel.find({ username: regex });
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 })
 
 
 
 router.get('/edit', IsLoggedIn, async function(req, res) {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
-    res.render('edit', { footer: true, loginuser });
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        res.render('edit', { footer: true, loginuser });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+
 });
 
+
 router.get('/upload', IsLoggedIn, async function(req, res) {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
-    res.render('upload', { footer: true, loginuser });
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        res.render('upload', { footer: true, loginuser });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 });
 
 
 router.post(`/upload/post`, IsLoggedIn, upload.single(`image`), async(req, res) => {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
+    try {
 
-    const createdpost = await postModel.create({
-        caption: req.body.caption,
-        image: req.file.filename,
-        user: loginuser,
-    })
+        if (!req.body.caption || !req.file) {
+            return res.status(400).json({ success: false, message: "Caption is required and image must be selected" });
+        }
 
-    loginuser.posts.push(createdpost);
-    await loginuser.save();
+        if (!req.body.caption) {
+            return res.status(400).json({ success: false, message: "Caption is required for a post" });
+        }
 
-    res.redirect(`/feed`);
+        if (!req.file) {
+            return res.status(400).json({ success: false, message: "Image is required for a post" });
+
+        }
+
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+
+        const createdpost = await postModel.create({
+            caption: req.body.caption,
+            image: req.file.filename,
+            user: loginuser,
+        })
+
+        loginuser.posts.push(createdpost);
+        await loginuser.save();
+
+        res.redirect(`/feed`);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 })
 
 
 
 router.get(`/like/post/:postId`, IsLoggedIn, async(req, res) => {
 
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
-    const post = await postModel.findById({ _id: req.params.postId }).populate(`user`);
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        const post = await postModel.findById({ _id: req.params.postId }).populate(`user`);
 
-    if (post.likes.indexOf(loginuser._id) === -1) {
-        post.likes.push(loginuser._id);
+        if (post.likes.indexOf(loginuser._id) === -1) {
+            post.likes.push(loginuser._id);
 
-    } else {
-        post.likes.splice(post.likes.indexOf(loginuser._id), 1);
+        } else {
+            post.likes.splice(post.likes.indexOf(loginuser._id), 1);
 
+        }
+
+        await post.save();
+        await loginuser.save();
+
+        res.json(post);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
     }
-
-    await post.save();
-    await loginuser.save();
-
-    res.json(post);
 })
 
 
 router.get(`/openprofile/:username`, IsLoggedIn, async(req, res) => {
-    const loginuser = await userModel.findOne({ username: req.session.passport.user });
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user });
 
-    const openuser = await userModel.findOne({ username: req.params.username }).populate(`posts`);
+        const openuser = await userModel.findOne({ username: req.params.username }).populate(`posts`);
 
-    res.render(`openprofile`, { footer: true, loginuser, openuser });
+        res.render(`openprofile`, { footer: true, loginuser, openuser });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 })
 
 
 
 
 router.get(`/save/:postId`, IsLoggedIn, async(req, res) => {
-    const user = await userModel.findOne({ username: req.session.passport.user });
-    const post = await postModel.findById({ _id: req.params.postId });
+    try {
+        const user = await userModel.findOne({ username: req.session.passport.user });
+        const post = await postModel.findById({ _id: req.params.postId });
 
-    if (user.savedPosts.indexOf(post._id) === -1) {
-        user.savedPosts.push(post._id);
+        if (user.savedPosts.indexOf(post._id) === -1) {
+            user.savedPosts.push(post._id);
 
-    } else {
-        user.savedPosts.splice(user.savedPosts.indexOf(post._id), 1);
+        } else {
+            user.savedPosts.splice(user.savedPosts.indexOf(post._id), 1);
+        }
+
+
+        if (post.savedBy.indexOf(user._id) === -1) {
+            post.savedBy.push(user._id);
+        } else {
+            post.savedBy.splice(post.savedBy.indexOf(user._id), 1);
+        }
+
+        await user.save();
+        await post.save();
+
+        res.json(post);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
     }
-
-
-    if (post.savedBy.indexOf(user._id) === -1) {
-        post.savedBy.push(user._id);
-    } else {
-        post.savedBy.splice(post.savedBy.indexOf(user._id), 1);
-    }
-
-    await user.save();
-    await post.save();
-
-    res.json(post);
 
 })
 
@@ -201,79 +286,90 @@ router.get(`/save/:postId`, IsLoggedIn, async(req, res) => {
 
 
 router.post(`/comment/:data/:postid`, IsLoggedIn, async(req, res) => {
-    const commentpost = await postModel.findOne({ _id: req.params.postid })
-    const loginuser = await userModel.findOne({ username: req.session.passport.user });
+    try {
+        const commentpost = await postModel.findOne({ _id: req.params.postid })
+        const loginuser = await userModel.findOne({ username: req.session.passport.user });
 
-    const createdcomment = await commentModel.create({
-        text: req.params.data,
-        user: loginuser._id,
-        post: commentpost._id
-    });
+        const createdcomment = await commentModel.create({
+            text: req.params.data,
+            user: loginuser._id,
+            post: commentpost._id
+        });
 
-    commentpost.comments.push(createdcomment._id);
+        commentpost.comments.push(createdcomment._id);
 
-    await commentpost.save();
-    await loginuser.save();
+        await commentpost.save();
+        await loginuser.save();
 
-    // const allcomments = await commentModel.find({ post: commentpost._id }).populate(`user`);
-    const onecomment = await commentModel.findOne({ _id: createdcomment._id }).populate(`user`).populate(`post`);
-    res.json(onecomment);
-
-})
-
-
-router.get(`/comments/:postid`, async(req, res, next) => {
-    const Post = await postModel.findById({ _id: req.params.postid });
-    const comments = await commentModel.find({ post: Post._id }).populate(`user`);
-    res.json(comments);
-
-})
-
-
-router.get(`/previous/comments/:post`, async(req, res, next) => {
-    const Post = await postModel.findById({ _id: req.params.post });
-    const previousComments = await commentModel.find({ post: Post._id }).populate(`user`);
-    if (previousComments.length > 0) {
-        res.json(previousComments);
+        // const allcomments = await commentModel.find({ post: commentpost._id }).populate(`user`);
+        const onecomment = await commentModel.findOne({ _id: createdcomment._id }).populate(`user`).populate(`post`);
+        res.json(onecomment);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
     }
 
 })
+
+
+
 
 // follow and unfollow a user 
 
 router.put(`/follow/:followeruser`, IsLoggedIn, async function(req, res, next) {
-    const followeduser = await userModel.findOne({ username: req.params.followeruser });
+    try {
+        const followeduser = await userModel.findOne({ username: req.params.followeruser });
 
-    const followinguser = await userModel.findOne({ username: req.session.passport.user });
+        const followinguser = await userModel.findOne({ username: req.session.passport.user });
 
-    if (followeduser.followers.indexOf(followinguser._id) === -1) {
-        followeduser.followers.push(followinguser._id);
-    } else {
-        followeduser.followers.splice(followinguser.followers.indexOf(followinguser._id), 1);
+        if (followeduser.followers.indexOf(followinguser._id) === -1) {
+            followeduser.followers.push(followinguser._id);
+        } else {
+            followeduser.followers.splice(followinguser.followers.indexOf(followinguser._id), 1);
+        }
+
+
+        if (followinguser.following.indexOf(followeduser._id) === -1) {
+            followinguser.following.push(followeduser._id);
+        } else {
+            followinguser.following.splice(followeduser.following.indexOf(followinguser._id), 1);
+        }
+
+        await followeduser.save();
+        await followinguser.save();
+
+        res.json(followinguser);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
     }
+})
 
 
-    if (followinguser.following.indexOf(followeduser._id) === -1) {
-        followinguser.following.push(followeduser._id);
-    } else {
-        followinguser.following.splice(followeduser.following.indexOf(followinguser._id), 1);
+
+router.get(`/view/comments/:postId`, IsLoggedIn, async(req, res, next) => {
+    try {
+        const post = await postModel.findById({ _id: req.params.postId });
+        const comments = await commentModel.find({ post: post._id }).populate(`user`);
+        const loginuser = await userModel.findOne({ username: req.session.passport.user });
+        res.render(`comments`, { header: true, loginuser, comments, post })
+    } catch (err) {
+        return res.status(500).json({ success: false, message: err.message })
+
     }
-
-    await followeduser.save();
-    await followinguser.save();
-
-    res.json(followinguser);
 })
 
 
 
 
 router.get(`/post/likes/:postId`, IsLoggedIn, async(req, res) => {
-    const post = await postModel.findById({ _id: req.params.postId }).populate(`likes`).populate(`user`);
+    try {
+        const post = await postModel.findById({ _id: req.params.postId }).populate(`likes`).populate(`user`);
 
-    const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
 
-    res.render(`likedby`, { loginuser, post, footer: true })
+        res.render(`likedby`, { loginuser, post, footer: true })
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
 
 });
 
@@ -295,7 +391,6 @@ router.get(`/post/likes/users/:postId/:input`, IsLoggedIn, async(req, res) => {
 });
 
 
-
 router.get('/followers/:userId', async(req, res) => {
     try {
         const loginuser = await userModel.findOne({ username: req.session.passport.user })
@@ -306,7 +401,7 @@ router.get('/followers/:userId', async(req, res) => {
         res.status(200).send({ message: "Error while fetching the followers of this user" })
     }
 });
-roadcls
+
 
 router.get('/followings/:userId', async(req, res) => {
     try {
@@ -346,7 +441,6 @@ router.get(`/search/:openuser/followers/:input`, IsLoggedIn, async(req, res) => 
 
 
 
-
 router.get(`/search/:openuser/following/:input`, IsLoggedIn, async(req, res) => {
     try {
         const openUser = req.params.openuser;
@@ -366,6 +460,28 @@ router.get(`/search/:openuser/following/:input`, IsLoggedIn, async(req, res) => 
     }
 });
 
+router.put(`/comment/like/:commentID`, IsLoggedIn, async(req, res, next) => {
+    try {
+        const loginuser = await userModel.findOne({ username: req.session.passport.user })
+        const comment = await commentModel.findOne({ _id: req.params.commentID });
+        const length = comment.likes.length;
+
+
+        if (comment.likes.indexOf(loginuser._id) === -1) {
+            comment.likes.push(loginuser._id);
+
+        } else {
+            comment.likes.splice(comment.likes.indexOf(loginuser._id), 1);
+        }
+
+        await loginuser.save()
+        await comment.save();
+        res.status(200).json({ success: true, length });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error });
+
+    }
+})
 
 
 module.exports = router;
